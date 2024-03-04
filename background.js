e6ea702d1f383f5b96631432c30cd872008bcb47
YK <1811651+ykdojo@users.noreply.github.com>
2024-03-03 18:25:56 -0800

    Removed inject.js to incorporate it into background.js cleaned up manifest.json

--- background.js
+++ background.js
@@ -1,9 +1,22 @@
 // this is the background code...
-
 // listen for our browerAction to be clicked
-chrome.browserAction.onClicked.addListener(function (tab) {
-	// for the current tab, inject the "inject.js" file & execute it
-	chrome.tabs.executeScript(tab.ib, {
-		file: 'inject.js'
-	});
+chrome.browserAction.onClicked.addListener(function () {
+	function openChatWindow(url) {
+		// Desired width and height of the window
+		const width = 545;
+		const height = 600;
+	
+		// Calculate the position from the right edge of the screen
+		const left = window.screen.width - width;
+		const top = 0;
+	
+		// Set the desired features for the pop-out window including size and location
+		const features = `location=no,menubar=no,toolbar=no,width=${width},height=${height},left=${left},top=${top}`;
+	
+		// Open the new window with the specified features
+		window.open(url, 'ChatWindow', features);
+	}
+	
+	// Usage: call this function with the URL of the chat
+	openChatWindow('https://www.twitch.tv/popout/ykdojo/chat');
 });
