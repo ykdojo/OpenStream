dc57aee519c0250158191b90812cfdda1dac2c02
YK <1811651+ykdojo@users.noreply.github.com>
2024-03-16 11:58:13 -0700

    Hide Twitter DM drawer

---
+++ hideTwitterDMDrawer.js
@@ -0,0 +1,4 @@
+var style = document.createElement('style');
+style.type = 'text/css';
+style.innerHTML = '[data-testid="DMDrawer"] { display: none !important; }';
+document.head.appendChild(style);
