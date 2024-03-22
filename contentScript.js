71082ddd01ee80d9e9f5adef9450ee3100a22890
YK <1811651+ykdojo@users.noreply.github.com>
2024-03-22 16:50:18 -0700

    Test finding the key element for Twitch CRM

--- contentScript.js
+++ contentScript.js
@@ -2,3 +2,11 @@ const header = document.querySelector('.stream-chat-header');
 if (header) {
   header.parentNode.removeChild(header);
 }
+
+// document.querySelectorAll('div[data-a-target="tw-core-button-label-text"]').forEach(function(element) {
+//   if (element.textContent.trim() === "Invite") {
+//       console.log('element found')
+//       console.log(element)
+//       element.style.color = 'red';
+//   }
+// });
