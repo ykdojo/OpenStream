90292224023a41217ac9e6ef57371ab2ad8077cb
YK <1811651+ykdojo@users.noreply.github.com>
2024-03-22 17:00:40 -0700

    Identify the right box where we want to insert our own button

--- contentScript.js
+++ contentScript.js
@@ -3,10 +3,18 @@ if (header) {
   header.parentNode.removeChild(header);
 }
 
-// document.querySelectorAll('div[data-a-target="tw-core-button-label-text"]').forEach(function(element) {
-//   if (element.textContent.trim() === "Invite") {
-//       console.log('element found')
-//       console.log(element)
-//       element.style.color = 'red';
-//   }
-// });
+document.querySelectorAll('div[data-a-target="tw-core-button-label-text"]').forEach(function(element) {
+  if (element.textContent.trim() === "Invite") {
+      console.log('element found');
+      let parent = element.parentElement;
+      while (parent) {
+          // Get all direct children
+          let childDivs = Array.from(parent.children).filter(child => child.tagName === 'DIV');
+          if (parent.tagName.toLowerCase() === 'div' && childDivs.length >= 4) {
+              console.log('Parent div with 4 or more direct child divs:', parent);
+              break; // Exit the loop as we found the target parent
+          }
+          parent = parent.parentElement;
+      }
+  }
+});
